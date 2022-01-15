71cbab37fe3c23a8418bab4e3da0667ae759548c
chaoticgoodpanda <michael.lwin@koekoetech.com>
2022-01-15 20:43:00 +0100

    Added keys.js to store Google OAuth stuff

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // create express app - used to setup config to listen for requests routed to express app from node side
 // and route to different route handlers
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'hello earthling' })
-});
+passport.use(new GoogleStrategy());
 
 // whenever Heroku runs our app it has ability to inject env variables that are in the JS runtime
 // node runs on top of. Heroku only sets port at very end, so we need to check then
