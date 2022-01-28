707c9c3781862920cc17bc3d7d3af5240b36edbe
chaoticgoodpanda <michael.lwin@koekoetech.com>
2022-01-28 23:30:45 +0100

    Added gitignore files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,22 @@
-// if any file is exporting a class or a component, it gets named a capital letter (App.js)
-// but if it's just a function, lowercase (index.js)
-
 import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import Header from './Header';
 
-const Header = () => <h2>Header</h2>
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
     return (
         <div>
-            Hi There!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path='/' component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {applyMiddleware, createStore} from "redux";
 import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
 
 import App from "./components/App";
 import reducers from './reducers';
+import {BrowserRouter} from "react-router-dom";
 
 const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
     // wrap the App in the Provider tag
-    <Provider store={store}><App /></Provider>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
 
